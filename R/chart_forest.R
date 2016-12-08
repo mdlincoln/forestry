f6b9245cf02389ad7a3acb8badd46982af521881
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-12-08 15:32:38 -0800

    working dual variables

--- R/chart_forest.R
+++ R/chart_forest.R
@@ -8,20 +8,26 @@
 #' @export
 chart_forest <- function(sim_data, log_var1 = TRUE) {
   #stopifnot(inherits(sim_data, "lumberjackData"))
-  varnames <- names(sim_data)[-ncol(sim_data)]
-  var1 <- varnames[1]
-  var2 <- varnames[2]
+
+  if (attr(sim_data, "n_vars") == 1) {
+    var1 <- names(sim_data)[1]
+    var2 <- NULL
+  } else {
+    var1 <- names(sim_data)[1]
+    var2 <- names(sim_data)[2]
+  }
+
+  print(map(sim_data, class))
 
   p <- ggplot(sim_data, aes_(x = as.name(var1), y = ~preds)) +
     scale_color_brewer(type = "qual") +
     theme_bw(base_size = 18) +
-    ylim(0, 1) +
     labs(y = "Probability of falling to selected class")
 
   if (log_var1)
     p <- p + scale_x_log10(labels = scales::comma)
 
-  if (is.na(var2)) {
+  if (is.null(var2)) {
     p <- p + geom_line()
   } else {
     p <- p + geom_line(aes_(color = as.name(var2)))
@@ -45,22 +51,26 @@ chart_forest <- function(sim_data, log_var1 = TRUE) {
 #'   progress bar will be displayed while simulating new data.
 #'
 #' @export
-simulate_data <- function(rf, d, class, var1, breaks1 = 50, var2 = NULL, breaks2 = NULL, shiny_session = NULL, ...) {
+simulate_data <- function(rf, d, class, var1, breaks1 = 50, var2 = NULL, breaks2 = 5, shiny_session = NULL, ...) {
   if (is.null(var2)) {
-    sd <- simulate_data1(rf, d, class, var1, breaks1, ...)
+    sd <- simulate_data1(rf, d, class, var1, breaks1, shiny_session = shiny_session, ...)
+    attr(sd, "n_vars") <- 1
   } else {
-    sd <- simulate_data2(rf, d, class, var1, breaks1, var2, breaks2, ...)
+    sd <- simulate_data2(rf, d, class, var1, breaks1, var2, breaks2, shiny_session = shiny_session, ...)
+    attr(sd, "n_vars") <- 2
   }
   class(sd) <- c(class(sd), "lumberjackData")
   sd
 }
 
-simulate_data1 <- function(rf, d, class, var1, breaks1, shiny_session = NULL, ...) {
+simulate_data1 <- function(rf, d, class, var1, breaks1, shiny_session, ...) {
 
   sim_var1 <- quantile(d[[var1]], seq(0, 1, length.out = min(dplyr::n_distinct(d[[var1]]), breaks1)))
 
-  if (!is.null(shiny_session))
-    pb <- Progress$new(shiny_session, min = 1, max = length(sim_var1), message = "Simulating new data...")
+  if (!is.null(shiny_session)) {
+    pb <- Progress$new(shiny_session, min = 1, max = length(sim_var1))
+    pb$set(message = "Simulating new data...")
+  }
 
   new_d <- purrr::map_df(sim_var1, function(x) {
     d[[var1]] <- x
@@ -77,15 +87,18 @@ simulate_data1 <- function(rf, d, class, var1, breaks1, shiny_session = NULL, ..
   new_d
 }
 
-simulate_data2 <- function(rf, d, class, var1, breaks1, var2, breaks2 = 5, shiny_session = NULL) {
+simulate_data2 <- function(rf, d, class, var1, breaks1, var2, breaks2, shiny_session) {
   sim_var1 <- quantile(d[[var1]], seq(0, 1, length.out = min(dplyr::n_distinct(d[[var1]]), breaks1)))
 
-  sim_var2 <- quantize_vector(d, var2, breaks2)
+  sim_var2 <- d[[var2]]
 
   combos <- purrr::cross2(sim_var1, sim_var2)
+  print(length(combos))
 
-  if (!is.null(shiny_session))
-    pb <- Progress$new(shiny_session, min = 1, max = length(combos), message = "Simulating new data...")
+  if (!is.null(shiny_session)) {
+    pb <- Progress$new(shiny_session, min = 0, max = 1)
+    pb$set(message = "Simulating new data...")
+  }
 
   new_d <- purrr::map_df(combos, function(x) {
     d[[var1]] <- x[[1]]
@@ -94,20 +107,23 @@ simulate_data2 <- function(rf, d, class, var1, breaks1, var2, breaks2 = 5, shiny
     sim_d <- data.frame(var1 = x[[1]], var2 = x[[2]], preds = mean(preds[, class]))
     names(sim_d)[1:2] <- c(var1, var2)
     if (exists("pb"))
-      pb$inc(amount = 1)
+      pb$inc(amount = 1/length(combos))
 
     sim_d
   })
 
   if (exists("pb"))
     pb$close()
+
+  new_d[[var2]] <- cut(new_d[[var2]], breaks = quantile(new_d[[var2]], probs = seq(0, 1, length.out = breaks2)))
+
   new_d
 }
 
 # Utility to take a continuous variable and quantize it
 quantize_vector <- function(d, v, n_breaks) {
   if (!is.null(v) && is.numeric(d[[v]])) {
-    cut(d[[v]], breaks = quantile(d[[v]], probs = seq(0, 1, length.out = min(dplyr::n_distinct(d[[v]]) - 1, n_breaks))))
+    cut(d[[v]], breaks = quantile(d[[v]], probs = n_breaks))
   } else {
     d[[v]]
   }
--- tests/testthat/test_survey.R
+++ tests/testthat/test_survey.R
@@ -1,4 +1,5 @@
 library(randomForest)
 
 iris_rf <- randomForest(Species ~ ., data = iris)
+partialPlot(iris_rf, pred.data = iris[,-5], x.var = "Sepal.Length", which.class = "setosa")
 survey_forest(iris_rf, data = iris)
