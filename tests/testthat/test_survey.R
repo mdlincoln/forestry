eb9de45b0db3cceec37c998ac398c047bdd0801f
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-01-03 16:48:38 -0800

    test with simulate_data

--- tests/testthat/test_survey.R
+++ tests/testthat/test_survey.R
@@ -2,5 +2,8 @@ library(randomForest)
 
 iris_rf <- randomForest(Species ~ ., data = iris)
 partialPlot(iris_rf, pred.data = iris[,-5], x.var = "Sepal.Length", which.class = "setosa")
+simd <- simulate_data(iris_rf, d = iris, class = "setosa", var1 = "Sepal.Length")
+ggplot(simd, aes(x = Sepal.Length, y = preds)) +
+  geom_line()
 survey_forest(iris_rf, data = iris)
 
